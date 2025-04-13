7ac5072de52e9b1b60711ffd38d29fe144f5c60d
tejash2004re <tejashwin2004teju@gmail.com>
2025-04-13 21:58:59 +0530

    img

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./App.css";
 import Cake from './components/Cakes';
 import Footer from './components/Footer';
 import FeedbackForm from './components/FeedbackForm';
+import creatorImg from '../src/assets/tejashwin.jpg'; // Add your image path
 
 const Home = ({ user }) => {
+  const [showPopup, setShowPopup] = useState(true);
+
+  const handleClose = () => {
+    setShowPopup(false);
+  };
+
   return (
     <>
-      
-      <div className='main_img'>
-      </div>
+      {showPopup && (
+        <div className="popup-overlay">
+          <div className="popup-window">
+            <img src={creatorImg} alt="Creator" className="creator-img" />
+            <h2> Project Credits</h2>
+            <p>Developed by: <strong>TEJASHWIN S</strong></p>
+            <p>🔒 Disclaimer: It is a testing project created for learning purposes only.
+⚠️ Please do not enter any real personal information </p>
+            <p>Contact <strong><a href='https://www.linkedin.com/in/tejashwin2004/'>LinkedIn</a></strong></p>
+            <button onClick={handleClose}>Close</button>
+          </div>
+        </div>
+      )}
+
+      <div className='main_img'></div>
       <Cake />
-      <FeedbackForm/>
+      <FeedbackForm />
       <Footer />
     </>
   );
